7b29f7b2c89870326d9d4743541982c6839dbd04
elenaJO <ele.lucero1993@gmail.com>
2018-01-11 11:02:28 -0500

    añadiendo modificacion

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $('.button-collapse').sideNav();
   $('#foto').attr('src', localStorage.photo);
   var config = {
@@ -10,22 +10,16 @@ $(document).ready(function () {
     messagingSenderId: '445743781768'
   };
   firebase.initializeApp(config);
-  var $textArea = $('#textarea1');
-  var $postButton = $('#postButton');
-  // var $date = moment().format('LT');
-
-  var $valueTextTarea = $textArea.val();
-  var $postButton2 = $('button[type=submit]');
   // var dbRef = firebase.database().ref('usuarios');
-  $('#fileButton').change(function () {
+  $('#fileButton').change(function() {
     var file = event.target.files[0];
     var storageRef = firebase.storage().ref('/' + localStorage.name + '/' + file.name);
     var task = storageRef.put(file);
-    task.on('state_changed', function (snapshot) {
+    task.on('state_changed', function(snapshot) {
 
-    }, function (error) {
+    }, function(error) {
 
-    }, function (error) {
+    }, function(error) {
       var postKey = firebase.database().ref('Posts/').push().key;
       var downloadURL = task.snapshot.downloadURL;
       var updates = {};
@@ -39,50 +33,15 @@ $(document).ready(function () {
       // console.log(downloadURL);
     });
     var appen = '<div class="row">' +
-      '<div class="col s 12 align">' +
-      '<div style="display=inline-block" class="img-user"><img src="_photo_"></div>' + '<div class="div-name"><h4></h4></div>' + '<br>' +
-      '<img src="_pub_" alt="" class="img-pub">' +
-      '</div>' +
-      '<br>' + '<br>' +
-      '<a class="a-icon"><i class="material-icons">favorite</i></span></a>' +
-      '<a class="a-icon"><i class="material-icons">question_answer</i></span></a>' +
-      '<a class="a-icon rigth"><i class="material-icons">more_horiz</i></span></a>' +
-      '</div>' +
-      '<div></div>';
+                  '<div class="col s 12 align">' +
+                    '<img src="_pub_" alt="" class="img-pub">' +
+                  '</div>' +
+                '</div>'; 
     var reader = new FileReader();
-    reader.onload = function (event) {
-      var appenReplace = appen.replace('_pub_', event.target.result).replace('_photo_', localStorage.photo).replace('<h4></h4>', localStorage.name);
+    reader.onload = function(event) {
+      var appenReplace = appen.replace('_pub_', event.target.result);
       $('#publicaciones').append(appenReplace);
     };
     reader.readAsDataURL(this.files[0]);
-  });
-
-  $textArea.on('keyup', function (event) {
-    // Si textArea no contiene nada o contiene un vacío el boton se desabilita
-    if ($(this).val().length === 0 || $(this).val().length === ' ') {
-      $postButton2.attr('disabled', true); // desabilita el boton
-      // textArea contiene algo
-    } else if ($(this).val().length >= 1) {
-      $postButton2.attr('disabled', false); // habilita el boton
-    }
-  });
-
-  $postButton.on('click', function () {
-    var appen = '<div class="row">' +
-      '<div class="col s 12 align">' +
-      '<div style="display=inline-block" class="img-user"><img src="_photo_"></div>' + '<div class="div-name"><h4></h4></div>' + '<br>' +
-      '<li><span></span><li>' +
-      '<br>' + '<br>' +
-      '<a class="a-icon"><i class="material-icons">favorite</i></span></a>' +
-      '<a class="a-icon"><i class="material-icons">question_answer</i></span></a>' +
-      '<a class="a-icon rigth"><i class="material-icons">more_horiz</i></span></a>' +
-      '</div>' +
-      '<div></div>' +
-      '<br>';
-
-    var appenReplace = appen.replace('<span></span>', $textArea.val()).replace('_photo_', localStorage.photo).replace('<h4></h4>', localStorage.name);
-    $('#publicaciones').append(appenReplace);
-
-    $valueTextTarea = $textArea.val('');
   });
 });
