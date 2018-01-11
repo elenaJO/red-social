9a86082a8b8ccdacc2b999d3b755d297ed9f3d8a
elenaJO <ele.lucero1993@gmail.com>
2018-01-11 10:38:48 -0500

    añadiedndo primera parte

---
+++ js/login.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    window.location.href = '../views/profile.html';
+  }, 22000);
+
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyBTyPXp0vll8d2Fvi5nViLsKntlNxapEFY',
+    authDomain: 'red-social-a1aeb.firebaseapp.com',
+    databaseURL: 'https://red-social-a1aeb.firebaseio.com',
+    projectId: 'red-social-a1aeb',
+    storageBucket: 'red-social-a1aeb.appspot.com',
+    messagingSenderId: '445743781768'
+  };
+  firebase.initializeApp(config);
+
+  $('#login').click(function() {
+    google();
+    console.log('haha');
+  });
+
+  function google() {
+    // provedor del servicio
+    var provider = new firebase.auth.GoogleAuthProvider();
+    // levantar la ventana de gmail y trae un result
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      // guardando la imagen y nombre;
+      localStorage.photo = result.user.photoURL;
+      localStorage.name = result.user.displayName;
+      localStorage.id = result.user.uid;
+      console.log(result.user);
+      guardarFirebase(result.user);
+    });
+  }
+
+  // funcion para guardar en firebase los datos de quien entra
+  function guardarFirebase(user) {
+    var usuario = {
+      uid: user.uid,
+      nombre: user.displayName,
+      foto: user.photoURL,
+      mail: user.email,
+      seguidores: 31,
+    };
+    firebase.database().ref('usuarios/' + user.uid).set(usuario);
+    // window.location.href = '../views/profile.html';
+  }
+})
---
+++ js/profile.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+  $('.button-collapse').sideNav();
+  $('#name-user').text(localStorage.name);
+  $('#foto').attr('src', localStorage.photo);
+  var $seguidores = $('#seguidores');
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyBTyPXp0vll8d2Fvi5nViLsKntlNxapEFY',
+    authDomain: 'red-social-a1aeb.firebaseapp.com',
+    databaseURL: 'https://red-social-a1aeb.firebaseio.com',
+    projectId: 'red-social-a1aeb',
+    storageBucket: 'red-social-a1aeb.appspot.com',
+    messagingSenderId: '445743781768'
+  };
+  // console.log(localStorage.id);
+
+  firebase.initializeApp(config);
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      var token = firebase.auth().currentUser.uid;
+      queryDataset(token);
+    }
+  });
+
+  function queryDataset(token) {
+    firebase.database().ref('/Posts/').once('value').then(function (snapshot) {
+      var Postarray = snapshot.val();
+      var keys = Object.keys(Postarray);
+      for (var i = 0; i < keys.length; i++) {
+        var currentObject = Postarray[keys[i]];
+        if (currentObject.user === localStorage.id) {
+          var appen = '<div class="row">' +
+            '<div class="col s 12 align">' +
+            '<img src = "_perfil_" class="photo-perfil" >' +
+            '<span>_name_</span>' +
+            '<img src="_pub_" alt="" class="img-pub">' +
+            '</div>' +
+            '</div>' +
+            '<div></div>';
+          var appenReplace = appen.replace('_pub_', currentObject.url).replace('_perfil_', localStorage.photo).replace('_name_', localStorage.name);
+          $('#publicaciones').append(appenReplace);
+          console.log(currentObject.user);
+        }
+      }
+      // console.log(Object.keys(Postarray));
+      // console.log(Postarray);
+    });
+  }
+  // para traer de la base de datos el numero de seguidores
+  var dbRef = firebase.database().ref('usuarios');
+  var dbRefUsu = dbRef.child(localStorage.id);
+  dbRefUsu.on('value', function(snap) {
+    $seguidores.text((snap.val()['seguidores']));
+    // console.log(snap.val());
+  });
+
+  // aumentar o disminuir seguidores
+  $('#follow').click(function() {
+    $(this).toggleClass('followed');
+    var dbUserFollow = dbRefUsu.child('seguidores');
+    if ($(this).hasClass('followed')) {
+      $(this).text('Followed');
+      dbUserFollow.transaction(function (curentFollow) {
+        return curentFollow + 1;
+      });
+    } else {
+      $(this).text('Follow');
+      dbUserFollow.transaction(function (curentFollow) {
+        return curentFollow - 1;
+      });
+    }
+  });
+});
